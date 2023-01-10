b76cceb158443666298d782e09e4456090817986
Fidelis <fideliofidel9@gmail.com>
2023-01-10 12:40:16 +0300

    javascript-form-validation

---
+++ index.js
@@ -0,0 +1,64 @@
+const form = document.getElementById("survey-form");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const number = document.getElementById("number");
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = document.querySelector(".error");
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = document.querySelector(".error");
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+}
+
+const isValidEmail = email => {
+    const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    return re.text(String(email).toLowerCase());
+}
+
+
+const validateInputs = () => {
+    const nameValue = name.value.trim();
+    const emailValue = email.value.trim();
+    const numberValue = number.value.trim();
+
+    if (nameValue === '') {
+        setError(name, 'Name is required');
+    } else {
+        setSuccess(name);
+    }
+
+    if (emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address')
+    } else {
+        setSuccess(email);
+    }
+
+    if (numberValue === '') {
+        setError(number, 'Age is required');
+    } else if (numberValue.length > 2) {
+        setError(number, 'Age must be two digits')
+    } else {
+        setSuccess(number);
+    }
+}
